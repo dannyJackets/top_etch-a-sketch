6c4999dd2ae8a93c22d6db7438a5e35efc63ea08
dannyJackets <djcoats23@gmail.com>
2022-02-10 17:34:59 -0500

    added 'drawing' capability to pad and some styling/formatting. need to add functionality of erase button and user input for grid size as well as clean up design

--- javascript.js
+++ javascript.js
@@ -2,9 +2,16 @@ const GRID = 16*16; //global grid size value
 
 for(var i = 0; i < GRID; i++){
     const div = document.createElement("div");
+    /*
     const node = document.createTextNode(i);
     div.appendChild(node);
+    */
     div.className = "pixel";
+    div.id = "p" + i;
     document.getElementById("grid").appendChild(div);
+    document.getElementById(div.id).addEventListener("mouseover", function(e){ drawing(e) });
 }
 
+function drawing(e){
+    document.getElementById(e.target.id).style.backgroundColor = "black";
+}
