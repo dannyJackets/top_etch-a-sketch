83cf32bf20a5821e325bee7433d0a9f190f6f481
dannyJackets <djcoats23@gmail.com>
2022-02-14 18:00:58 -0500

    completed etch-a-sketch project per t.o.p. requirements (minus optional since would be simple to implement but not truly and 'etch-a-sketch' in function).

--- javascript.js
+++ javascript.js
@@ -1,17 +1,52 @@
-const GRID = 16*16; //global grid size value
+const MAX_DIMENSION = 640;  //max pixel dimension to fit in etch pad
+var GRID = 16*16; //global grid size value
 
-for(var i = 0; i < GRID; i++){
-    const div = document.createElement("div");
-    /*
-    const node = document.createTextNode(i);
-    div.appendChild(node);
-    */
-    div.className = "pixel";
-    div.id = "p" + i;
-    document.getElementById("grid").appendChild(div);
-    document.getElementById(div.id).addEventListener("mouseover", function(e){ drawing(e) });
-}
+
+generatePixels();
+
+document.getElementById("clear_grid").addEventListener("click", erase);
 
 function drawing(e){
     document.getElementById(e.target.id).style.backgroundColor = "black";
+}
+
+function erase(){   //erase colored pixels and start prompt for new size
+    const pixels = document.getElementsByClassName("pixel");
+    for(j = 0; j < GRID; j++){
+        pixels[j].style.backgroundColor = "white";
+    }
+    newDimensions();
+}
+
+function newDimensions(){   //user prompt for new grid size. also initates delete of existing divs and generation of new divs
+    pixelsPerSide = prompt("How many squares per side do you want?\n(The maximum is 100)", "16");
+    pixelInputCheck();
+    removePixels(document.getElementById("grid"));
+    GRID = pixelsPerSide * pixelsPerSide;
+    generatePixels();
+    document.getElementById("grid").style.gridTemplateColumns = 'repeat(' + pixelsPerSide + ', ' + MAX_DIMENSION / pixelsPerSide + 'px)';
+    document.getElementById("grid").style.gridTemplateRows = 'repeat(' + pixelsPerSide + ', ' + MAX_DIMENSION / pixelsPerSide + 'px)';
+}
+
+function pixelInputCheck(){ //verifies user input
+    if(pixelsPerSide > 100 || pixelsPerSide < 1 || pixelsPerSide % 1 != 0 ){
+        pixelsPerSide = prompt("ERROR, please enter a valid dimension value (integer between 1 - 100)")
+        pixelInputCheck();
+    }
+}
+
+function generatePixels(){  //generate new pixel divs under "grid"
+    for(var i = 0; i < GRID; i++){
+        const div = document.createElement("div");
+        div.className = "pixel";
+        div.id = "p" + i;
+        document.getElementById("grid").appendChild(div);
+        document.getElementById(div.id).addEventListener("mouseover", function(e){ drawing(e) });
+    }
+}
+
+function removePixels(parent){  //removes all divs under parent
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
 }
